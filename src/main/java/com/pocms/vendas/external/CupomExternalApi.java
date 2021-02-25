900b7cf42099a237c8c4be3d563a3e67b0392a6b
thiagopereira <amoR2401>
2021-02-25 10:51:37 -0300

    Alteração de configurações.

--- src/main/java/com/pocms/vendas/external/CupomExternalApi.java
+++ src/main/java/com/pocms/vendas/external/CupomExternalApi.java
@@ -1,6 +1,5 @@
 package com.pocms.vendas.external;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,11 +10,15 @@ import java.util.Objects;
 @Component
 public class CupomExternalApi {
 
-    @Autowired private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    public CupomExternalApi(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public BigDecimal getDescontoByCupom(@NotNull String cupom) {
         Objects.requireNonNull(cupom, "Código do cupom não pode ser nulo");
-        String url = "http://cupons/" + cupom;
+        String url = "http://ms-cupom.production.svc.cluster.local:30337/" + cupom;
         return restTemplate.getForObject(url, BigDecimal.class);
     }
 
